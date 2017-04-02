4916be8afc00685811b9bd933058726772b9568d
Rocky Gray <rocky.grayjr@gmail.com>
2017-04-02 03:11:23 -0400

    format money

--- src/main/java/com/mcordero/interviews/TransactionSummary.java
+++ src/main/java/com/mcordero/interviews/TransactionSummary.java
@@ -2,15 +2,18 @@ package com.mcordero.interviews;
 
 import javax.json.Json;
 import javax.json.JsonObject;
+import java.text.NumberFormat;
 
 public class TransactionSummary {
 
   private double credits;
   private double debits;
+  private NumberFormat formatter;
 
   public TransactionSummary(double credits, double debits) {
     this.credits = credits;
     this.debits = debits;
+    this.formatter = NumberFormat.getCurrencyInstance();
   }
 
   public double getCredits() {
@@ -23,8 +26,8 @@ public class TransactionSummary {
 
   public JsonObject toJson() {
     return Json.createObjectBuilder()
-      .add("spent", this.debits)
-      .add("income", this.credits)
+      .add("spent", this.formatter.format(this.debits))
+      .add("income", this.formatter.format(this.credits))
       .build();
   }
 
